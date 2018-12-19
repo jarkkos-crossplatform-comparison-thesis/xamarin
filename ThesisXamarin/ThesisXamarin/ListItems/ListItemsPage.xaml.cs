bcd50523c75e204db4cca03208375ebf3134b731
Jarkko Saarinen <jarkko.saarinen@uta.fi>
2018-12-19 19:42:23 +0200

    Remove select test page scrollability, remove loading indicator from list item page

--- ThesisXamarin/ThesisXamarin/ListItems/ListItemsPage.xaml.cs
+++ ThesisXamarin/ThesisXamarin/ListItems/ListItemsPage.xaml.cs
@@ -18,14 +18,12 @@ namespace ThesisXamarin.Pages
         public ListItemsPage(ListItemsLoader itemsLoader)
         {
             InitializeComponent();
-            LoadingItemsIndicator.IsVisible = true;
             loadItems(itemsLoader);
         }
 
         private async void loadItems(ListItemsLoader itemsLoader)
         {
             ListItems = await itemsLoader.LoadItems();
-            LoadingItemsIndicator.IsVisible = false;
             ItemListView.ItemsSource = ListItems;
         }
     }
